5897b055bc5841ac19f718341116c7e895aa6ec6
Darlan Freire <56272477+Darlan-Freire@users.noreply.github.com>
2019-11-09 19:26:10 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+function mudaImagem(foto){
+    document.getElementById("eu").src="Imagem/" + foto + ".png";
+};
+
+function certificado(nome) {
+    if(nome == 'python'){
+        window.open('https://www.cursoemvideo.com/wp-content/uploads/2018/08/7180-180330.jpeg','_blank');
+    } else
+    if(nome == 'java'){
+        window.open('https://www.cursoemvideo.com/wp-content/uploads/2019/02/2426-180330.jpeg','_blank');
+    } else
+    if(nome == 'mysql'){
+        window.open('https://www.cursoemvideo.com/certificates/certificado-curso-em-video-2/?c=1091&u=180330','_blank');
+    } else
+    if(nome == 'cisco'){
+        window.open('https://drive.google.com/file/d/1jVX0d2YU-DNTdVV5p2UzNYrA9GS9Il8m/view','_blank');
+    } else
+    if(nome == 'algoritmo'){
+        window.open('https://www.cursoemvideo.com/certificates/certificado-curso-em-video-2/?c=973&u=180330','_blank');
+    } else
+    if(nome == 'manutencao'){
+        window.open('https://drive.google.com/file/d/1SuH3zb2zbPCnXQYrSwjszcN-0KWCIN5Q/view','_blank');
+    } else
+    if(nome == 'c'){
+        alert('Tenho bons conhecimentos em linguagem C, desenvolvendo programas e atividades práticas na instituição de ensino.');
+    } else
+    if(nome == 'html'){
+        window.open('https://www.cursoemvideo.com/wp-content/uploads/2019/11/992-180330.jpeg','_blank');
+    } else
+    if(nome == 'cisco'){
+        window.open('https://drive.google.com/file/d/1jVX0d2YU-DNTdVV5p2UzNYrA9GS9Il8m/view','_blank');
+    };
+};
